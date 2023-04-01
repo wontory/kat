8813c6b8251f02361e42476a964ddb01d0309599
Wontory <tjddnjsjo@kakao.com>
2023-04-01 19:05:22 +0900

    feat(UI): add common UI component

---
+++ kat-v2/src/components/UI/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+function Card({ children }) {
+  return (
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">{children}</div>
+    </div>
+  );
+}
+
+export default Card;
