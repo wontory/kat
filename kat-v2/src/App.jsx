bd3b3174cf8aac3997a6c47e31690aac6d53083f
Wontory <tjddnjsjo@kakao.com>
2023-04-09 07:02:52 +0900

    refactor: remove server

--- kat-v2/src/App.jsx
+++ kat-v2/src/App.jsx
@@ -1,26 +1,106 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
 import NavigationBar from "./components/ui/NavigationBar";
 import LecturesList from "./components/Lectures/LecturesList";
 import NewLecture from "./components/NewLecture/NewLecture";
 
 const App = () => {
-  useEffect(() => {
-    callApi();
-  }, []);
+  const DUMMY_LECTURES = [
+    {
+      id: "l1",
+      name: "블록체인DApp설계",
+      credit: 3,
+      divisions: [
+        {
+          id: "d1",
+          day: "월",
+          time: "123",
+          professor: "윤익준",
+        },
+      ],
+    },
+    {
+      id: "l2",
+      name: "기초선형대수학",
+      credit: 3,
+      divisions: [
+        {
+          id: "d1",
+          day: "화",
+          time: "123",
+          professor: "박차경",
+        },
+        {
+          id: "d2",
+          day: "화",
+          time: "678",
+          professor: "박차경",
+        },
+        {
+          id: "d3",
+          day: "금",
+          time: "123",
+          professor: "허난",
+        },
+        {
+          id: "d4",
+          day: "금",
+          time: "678",
+          professor: "허난",
+        },
+      ],
+    },
+    {
+      id: "l3",
+      name: "알고리듬",
+      credit: 3,
+      divisions: [
+        {
+          id: "d1",
+          day: "월",
+          time: "678",
+          professor: "배상원",
+        },
+        {
+          id: "d2",
+          day: "화",
+          time: "123",
+          professor: "배상원",
+        },
+        {
+          id: "d3",
+          day: "수",
+          time: "123",
+          professor: "배상원",
+        },
+      ],
+    },
+    {
+      id: "l4",
+      name: "컴퓨터구조",
+      credit: 3,
+      divisions: [
+        {
+          id: "d1",
+          day: "화",
+          time: "123",
+          professor: "안진호",
+        },
+        {
+          id: "d2",
+          day: "수",
+          time: "678",
+          professor: "안진호",
+        },
+      ],
+    },
+  ];
 
-  const [lectures, setLectures] = useState([]);
+  const [lectures, setLectures] = useState(DUMMY_LECTURES);
 
   const addLectureHandler = (lecture) => {
     setLectures((prevLectures) => [...prevLectures, lecture]);
     console.log(lectures);
-  };
-
-  const callApi = async () => {
-    axios.get("/api").then((res) => {
-      setLectures(res.data);
-    });
   };
 
   return (
--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const test = require("./Router/test");
-
-app.use("/api", test);
-
-const port = 5000;
-app.listen(port, () => console.log(`${port}`));
