27e58ed10ba47e4444a738d15e08a5b4916c8f09
Wontory <tjddnjsjo@kakao.com>
2023-04-11 20:19:06 +0900

    refactor: generate algorithm

--- kat-v2/src/lib/generate.js
+++ kat-v2/src/lib/generate.js
@@ -2,41 +2,35 @@ let timetables = [];
 let timetableBuffer = [];
 
 const generate = (lectures, depth) => {
-  if (depth === 0) {
-    timetables = [];
-    timetableBuffer = [];
-  }
-
   if (depth === lectures.length) {
     timetables.push([...timetableBuffer]);
     return;
   }
 
-  for (let i = 0; i < lectures[depth].divisions.length; i++) {
-    let isReserved = false;
-    for (let j = 0; j < depth; j++) {
-      if (
-        (lectures[depth].divisions[i].time.includes(
-          timetableBuffer[j].time.charAt(0)
-        ) ||
-          timetableBuffer[j].time.includes(
-            lectures[depth].divisions[i].time.charAt(0)
-          )) &&
-        lectures[depth].divisions[i].day === timetableBuffer[j].day
-      ) {
-        isReserved = true;
-        break;
-      }
-    }
+  if (depth === 0) {
+    timetables = [];
+    timetableBuffer = [];
+  }
+
+  for (const division of lectures[depth].divisions) {
+    let isReserved = timetableBuffer.some((lecture) => {
+      return (
+        division.day === lecture.day &&
+        (division.time.includes(lecture.time.charAt(0)) ||
+          lecture.time.includes(division.time.charAt(0)))
+      );
+    });
 
     if (!isReserved) {
       timetableBuffer[depth] = {
-        ...lectures[depth].divisions[i],
-        id: lectures[depth].id + lectures[depth].divisions[i].id,
+        ...division,
+        id: lectures[depth].id + division.id,
         name: lectures[depth].name,
         credit: lectures[depth].credit,
       };
+
       generate(lectures, depth + 1);
+      timetableBuffer[depth] = {};
     }
   }
 
