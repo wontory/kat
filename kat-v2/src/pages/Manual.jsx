badd2c5e2841383e174a1ee141841893e711c04c
Wontory <tjddnjsjo@kakao.com>
2023-04-11 16:38:07 +0900

    refactor: flex direction > flex wrap

--- kat-v2/src/pages/Manual.jsx
+++ kat-v2/src/pages/Manual.jsx
@@ -5,7 +5,7 @@ import Comments from "../components/Comments/Comments";
 const Manual = () => {
   return (
     <div
-      className="flex flex-col p-8 gap-8 z-10 scrollbar-hide lg:flex-row"
+      className="flex flex-wrap p-8 gap-8 z-10 scrollbar-hide lg:flex-nowrap"
       style={{ height: "calc(100vh - 65px)" }}
     >
       <div className="card w-full max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide lg:w-1/2">
@@ -14,6 +14,19 @@ const Manual = () => {
           <div className="w-full">
             <h1>각종 문의 및 버그 제보는 ~~~ 또는 댓글로 편하게 해주세요 😊</h1>
             <h1>이슈 또는 PR도 환영입니다!</h1>
+          </div>
+          <div className="divider" />
+          <h2 className="card-title">사용 방법</h2>
+          <div>
+            <ol>
+              <li>1. </li>
+              <li>2. </li>
+              <li>3. </li>
+              <li>4. </li>
+              <li>5. </li>
+              <li>6. </li>
+              <li>7. </li>
+            </ol>
           </div>
         </div>
       </div>
