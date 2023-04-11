a16f9141e830bba8f32ea0c29ae65f1fa13f982b
Wontory <tjddnjsjo@kakao.com>
2023-04-11 13:54:48 +0900

    style(Manual): edit for responsive web

--- kat-v2/src/pages/Manual.jsx
+++ kat-v2/src/pages/Manual.jsx
@@ -4,10 +4,10 @@ import Comments from "../components/Comments/Comments";
 const Manual = () => {
   return (
     <div
-      className="flex p-8 gap-8 z-10 scrollbar-hide"
+      className="flex flex-wrap p-8 gap-8 z-10 scrollbar-hide lg:flex-nowrap"
       style={{ height: "calc(100vh - 65px)" }}
     >
-      <div className="card w-1/2 max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide">
+      <div className="card w-full max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide lg:w-1/2">
         <div className="card-body">
           <h2 className="card-title">환영합니다!</h2>
           <div className="w-full max-w-xs">
@@ -20,7 +20,7 @@ const Manual = () => {
           </div>
         </div>
       </div>
-      <div className="card w-1/2 max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide">
+      <div className="card w-full max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide lg:w-1/2">
         <div className="card-body">
           <h2 className="card-title">댓글</h2>
           <Comments />
