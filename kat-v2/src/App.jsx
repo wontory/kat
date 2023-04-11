64aaeecdf546a4ad653b5c68ed79d2c78fbc6c6c
Wontory <tjddnjsjo@kakao.com>
2023-04-11 11:12:15 +0900

    feat: add user manual

--- kat-v2/src/App.jsx
+++ kat-v2/src/App.jsx
@@ -10,6 +10,20 @@ const App = () => {
   const [lectures, setLectures] = useState([]);
   const [timetables, setTimetables] = useState([]);
 
+  const showManualHandler = () => {
+    Swal.fire({
+      title: "KAT 사용법",
+      width: "100%",
+      html: `<p>1. <a href="https://kutis.kyonggi.ac.kr/webkutis/view/indexWeb.jsp" target="_blank">KUTIS</a> 또는 <a href="http://sugang.kyonggi.ac.kr/" target="_blank">경기대학교 수강신청 시스템</a>에서 개설된 강좌를 확인합니다.</p>
+      <p>2. 과목을 추가하고 수강할 과목의 정보를 입력합니다.</p>
+      <p>3. 시간표 생성에 고려할 분반의 정보를 입력합니다.</p>
+      <p style="font-size: 13px">* 분반: 같은 과목명을 갖고 있으나 교수나 요일, 시간 정보가 다른 강좌</p>
+      <p>4. 과목 입력이 모두 끝나면 시간표 생성을 눌러 가능한 시간표 조합을 확인합니다.</p>`,
+      showCloseButton: true,
+      confirmButtonText: "닫기",
+    });
+  };
+
   const addLectureHandler = (lecture) => {
     setLectures((prevLectures) => [...prevLectures, lecture]);
   };
@@ -18,36 +32,35 @@ const App = () => {
   const generateTimetableHandler = () => {
     setTimetables(generate(lectures, 0));
 
-    let text = "";
+    let html = "";
 
     timetables.forEach((timetable) => {
       timetable.forEach(
         (lecture) =>
-          (text += `${lecture.name}&nbsp;&nbsp;&nbsp;&nbsp;${lecture.day} ${lecture.time}&nbsp;&nbsp;&nbsp;&nbsp;${lecture.credit}&nbsp;&nbsp;&nbsp;&nbsp;${lecture.professor}<br>`)
+          (html += `${lecture.name}&nbsp;&nbsp;&nbsp;&nbsp;${lecture.day} ${lecture.time}&nbsp;&nbsp;&nbsp;&nbsp;${lecture.credit}&nbsp;&nbsp;&nbsp;&nbsp;${lecture.professor}<br>`)
       );
-      text += "<br><br>";
+      html += "<br><br>";
     });
 
     Swal.fire({
       title: "시간표",
-      html: text,
+      html: html,
       confirmButtonText: "확인",
     });
   };
 
   return (
     <>
-      <NavigationBar />
+      <NavigationBar
+        onShowManual={showManualHandler}
+        onGenerate={generateTimetableHandler}
+      />
       <div
         className="flex p-8 gap-8 overflow-x-scroll overflow-y-hidden z-10 scrollbar-hide"
         style={{ height: "calc(100vh - 65px)" }}
       >
         {lectures.length !== 0 && <LecturesList items={lectures} />}
-        <NewLecture
-          id={lectures.length + 1}
-          onAddLecture={addLectureHandler}
-          onGenerate={generateTimetableHandler}
-        />
+        <NewLecture id={lectures.length + 1} onAddLecture={addLectureHandler} />
       </div>
     </>
   );
