2478e9db69eb025a387d1296add3e658aac8710f
Wontory <tjddnjsjo@kakao.com>
2023-04-12 15:28:08 +0900

    style: use daisyui table component

--- kat-v2/src/components/Timetables/TimetableItem.jsx
+++ kat-v2/src/components/Timetables/TimetableItem.jsx
@@ -5,23 +5,29 @@ const TimetableItem = (props) => {
     <div className="card w-80 max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide">
       <div className="card-body">
         <h2 className="card-title">시간표 {props.id.substring(1)}</h2>
-        <table className="w-full max-w-xs text-center">
-          <tr>
-            <th>과목명</th>
-            <th>교시</th>
-            <th>학점</th>
-            <th>담댱교수</th>
-          </tr>
-          {props.item.map((lecture) => (
-            <tr key={lecture.id}>
-              <td>{lecture.name}</td>
-              <td>
-                {lecture.day} {lecture.time}
-              </td>
-              <td>{lecture.credit}</td>
-              <td>{lecture.professor}</td>
+        <table className="table table-compact w-full max-w-xs">
+          <thead>
+            <tr>
+              <th></th>
+              <th>과목명</th>
+              <th>교시</th>
+              <th>학점</th>
+              <th>담댱교수</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {props.item.map((lecture, index) => (
+              <tr key={lecture.id}>
+                <th>{index + 1}</th>
+                <td>{lecture.name}</td>
+                <td>
+                  {lecture.day} {lecture.time}
+                </td>
+                <td>{lecture.credit}</td>
+                <td>{lecture.professor}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
