f5c6a4c3ade302df6fd023c6d593cac9d4364ca4
Wontory <tjddnjsjo@kakao.com>
2023-04-13 01:11:32 +0900

    style: edit result component for responsive web

--- kat-v2/src/components/Timetables/TimetableItem.jsx
+++ kat-v2/src/components/Timetables/TimetableItem.jsx
@@ -2,33 +2,38 @@ import React from "react";
 
 const TimetableItem = (props) => {
   return (
-    <div className="card w-80 max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide">
+    <div className="card w-80 max-h-full bg-base-100 shadow-xl overflow-y-scroll scrollbar-hide sm:w-full">
       <div className="card-body">
         <h2 className="card-title">시간표 {props.id.substring(1)}</h2>
-        <table className="table table-compact w-full max-w-xs">
-          <thead>
-            <tr>
-              <th></th>
-              <th>과목명</th>
-              <th>교시</th>
-              <th>학점</th>
-              <th>담댱교수</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.item.map((lecture, index) => (
-              <tr key={lecture.id}>
-                <th>{index + 1}</th>
-                <td>{lecture.name}</td>
-                <td>
-                  {lecture.day} {lecture.time}
-                </td>
-                <td>{lecture.credit}</td>
-                <td>{lecture.professor}</td>
+        <div
+          className="overflow-x-scroll scrollbar-hide"
+          style={{ maxHeight: "calc(100vh - 228px)" }}
+        >
+          <table className="table table-compact w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>과목명</th>
+                <th>교시</th>
+                <th>학점</th>
+                <th>담댱교수</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {props.item.map((lecture, index) => (
+                <tr key={lecture.id}>
+                  <th>{index + 1}</th>
+                  <td>{lecture.name}</td>
+                  <td>
+                    {lecture.day} {lecture.time}
+                  </td>
+                  <td>{lecture.credit}</td>
+                  <td>{lecture.professor}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- kat-v2/src/pages/Result.jsx
+++ kat-v2/src/pages/Result.jsx
@@ -4,8 +4,8 @@ import TimetableItem from "../components/Timetables/TimetableItem";
 
 const Result = (props) => {
   return (
-    <div className="flex p-8 gap-8 overflow-x-scroll overflow-y-hidden z-10 scrollbar-hide">
-      <ul className="flex flex-wrap justify-center items-start gap-8 lg:flex-nowrap lg:justify-normal">
+    <div className="flex flex-wrap justify-center items-start p-8 gap-8 overflow-x-scroll overflow-y-hidden z-10 scrollbar-hide sm:flex-nowrap sm:justify-normal">
+      <ul className="flex flex-wrap justify-center items-start gap-8 sm:flex-nowrap sm:justify-normal">
         {props.timetables.map((timetable) => (
           <TimetableItem
             key={timetable.id}
