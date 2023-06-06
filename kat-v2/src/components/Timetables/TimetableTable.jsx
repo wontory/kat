79fbd7f51ec8df15f5f27735c5096a963746a047
Wontory <tjddnjsjo@kakao.com>
2023-06-07 07:04:02 +0900

    feat: 표 형식 시간표 추가

--- kat-v2/src/components/Timetables/TimetableTable.jsx
+++ kat-v2/src/components/Timetables/TimetableTable.jsx
@@ -1,86 +1,11 @@
 import React from "react";
 
-const TimetableTable = () => {
+import convert from "../../lib/convert";
+
+const TimetableTable = (props) => {
   return (
     <div className="overflow-x-scroll scrollbar-hide">
-      <table className="table table-zebra table-compact w-full text-center">
-        <thead>
-          <tr>
-            <th></th>
-            <th>월</th>
-            <th>화</th>
-            <th>수</th>
-            <th>목</th>
-            <th>금</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>1</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th>2</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th>3</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th>4</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th>5</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th>6</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td rowSpan={3} style={{ backgroundColor: "red" }}>
-              ㅎㅇ
-            </td>
-          </tr>
-          <tr>
-            <th>7</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th>8</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+      {convert(props.item)}
     </div>
   );
 };
---
+++ kat-v2/src/lib/convert.js
@@ -0,0 +1,61 @@
+const days = ["", "월", "화", "수", "목", "금"];
+
+const convert = (timetable) => {
+  const table = Array.from(Array(9), () =>
+    Array(6).fill({ credit: null, name: null })
+  );
+  const result = [];
+
+  timetable.map((tt) => {
+    const classInfo = { credit: tt.credit, name: tt.name };
+    const day = days.indexOf(tt.day);
+    const time = Array.from(tt.time, mapfn);
+
+    return time.map((time) => (table[time][day] = classInfo));
+  });
+
+  createThead(result);
+  createTbody(table, result);
+
+  return (
+    <table className="table table-zebra table-compact w-full text-center">
+      {result}
+    </table>
+  );
+};
+
+const mapfn = (arg) => Number(arg);
+
+const createThead = (result) => {
+  const data = [];
+
+  days.map((day) => data.push(<th>{day}</th>));
+
+  result.push(
+    <thead>
+      <tr>{data}</tr>
+    </thead>
+  );
+};
+
+const createTbody = (table, result) => {
+  const row = [];
+
+  for (let i = 1; i < table.length; i++) {
+    const data = [];
+
+    data.push(<th>{i}</th>);
+    for (let j = 1; j < table[i].length; j++) {
+      table[i][j].credit !== null
+        ? JSON.stringify(table[i - 1][j]) !== JSON.stringify(table[i][j]) &&
+          data.push(<td rowspan={table[i][j].credit}>{table[i][j].name}</td>)
+        : data.push(<td></td>);
+    }
+
+    row.push(<tr>{data}</tr>);
+  }
+
+  result.push(<tbody>{row}</tbody>);
+};
+
+export default convert;
