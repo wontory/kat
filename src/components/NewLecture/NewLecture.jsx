fb875ac686870c0f5578f336914ee132da633f31
조성원 <tjddnjsjo@kakao.com>
2023-07-27 17:45:13 +0900

    style: 테이블 아이템 색 재지정

--- src/components/NewLecture/NewLecture.jsx
+++ src/components/NewLecture/NewLecture.jsx
@@ -3,28 +3,16 @@ import React, { useState } from "react";
 import LectureForm from "./LectureForm";
 
 const colors = [
-  "bg-slate-500",
-  "bg-gray-500",
-  "bg-zinc-500",
-  "bg-neutral-500",
-  "bg-stone-500",
   "bg-red-500",
-  "bg-orange-500",
-  "bg-amber-500",
-  "bg-yellow-500",
-  "bg-lime-500",
-  "bg-green-500",
-  "bg-emerald-500",
-  "bg-teal-500",
   "bg-cyan-500",
-  "bg-sky-500",
-  "bg-blue-500",
-  "bg-indigo-500",
   "bg-violet-500",
-  "bg-purple-500",
-  "bg-fuchsia-500",
-  "bg-pink-500",
   "bg-rose-500",
+  "bg-emerald-500",
+  "bg-yellow-500",
+  "bg-slate-500",
+  "bg-green-200",
+  "bg-indigo-900",
+  "bg-stone-400",
 ];
 
 const NewLecture = (props) => {
@@ -41,7 +29,7 @@ const NewLecture = (props) => {
   const saveLectureDataHandler = (enteredLectureData) => {
     const lectureData = {
       id: `l${props.id}`,
-      color: colors[Math.floor(Math.random() * colors.length)],
+      color: colors[props.id],
       ...enteredLectureData,
     };
     props.onAddLecture(lectureData);
