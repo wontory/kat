75c3808530182e428cd9fdbe968e4edc9f8c6a9e
조성원 <tjddnjsjo@kakao.com>
2024-02-19 23:38:53 +0900

    chore: eslint 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    '@titicaca/eslint-config-triple',
+    '@titicaca/eslint-config-triple/requiring-type-checking',
+    '@titicaca/eslint-config-triple/frontend',
+    'prettier',
+    '@titicaca/eslint-config-triple/prettier',
+  ],
 }
